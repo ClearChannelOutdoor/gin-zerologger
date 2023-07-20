31dfcee65a2bed8387824d38b4879fbfa9ffddb4
Joshua Thomas <joshuathomas@clearchannel.com>
2023-07-20 14:06:46 -0700

    adding extensible way to include context settings in log output

--- example/request.sh
+++ example/request.sh
@@ -2,24 +2,29 @@
 
 # should be a log.Debug
 echo should log at debug level
-curl -XGET -H"X-Correlation-ID:testing" localhost:8080
+curl -XGET localhost:8080
 
 # should be a log.Warn
 echo
 echo should log at warn level
-curl -XGET -H"X-Correlation-ID:testing" localhost:8080/400
+curl -XGET localhost:8080/400
 
 # should be a log.Error
 echo
-echo should log at error level and include additional details
-curl -XGET -H"X-Correlation-ID:testing" localhost:8080/500
+echo should log at error level and include additional error details
+curl -XGET localhost:8080/500
 
-# should be a log.Debug
+# should be a log.Debug and include request body
 echo
 echo should log at debug level and include the request body
-curl -XPOST -H"X-Correlation-ID:testing" localhost:8080/logbody --data "{\"test\":true}"
+curl -XPOST localhost:8080/logbody --data "{\"test\":true}"
 
-# should be a log.Debug
+# should be a log.Debug with correlation ID
 echo
-echo should log at debug level
-curl -XGET -H"X-Correlation-ID:testing" localhost:8080
+echo should log at debug level with correlation ID
+curl -XGET -H"X-Correlation-ID:12345" localhost:8080
+
+# should be a log.Debug with context settings
+echo
+echo should log at debug level with additional gin context values
+curl -XGET localhost:8080/logcontext
